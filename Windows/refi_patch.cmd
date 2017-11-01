a1291801ae4a7189027d29fd7845f6b11948f515
Jared Beckham <jtbeckha@gmail.com>
2017-11-01 01:31:10 -0400

    Add some comments so I can remember what this does

--- Windows/refi_patch.cmd
+++ Windows/refi_patch.cmd
@@ -1,10 +1,12 @@
 :: This script patches the microsoft bootloader with refind so I can boot to Ubuntu
 :: Windows keeps overriding my patch when it updates so I am creating this script to easily repatch
-:: When prompted, answer that bootmgfw.efi is a file, and yes to overwrite S:\EFI\Microsoft\Boot\bootmgfw.efi.
+:: When prompted, answer that bootmgfw.efi is a file, and yes to overwrite S:\EFI\Microsoft\Boot\bootmgfw.efi. Purposely force some interactivity to reduce the chances of accidentally screwing up your computer :D
 
 mountvol S: /S
 S:
 cd EFI\Microsoft\Boot
+# Backup the microsoft bootloader
 xcopy bootmgfw.efi ..\bootmgfw.efi
+# Put refind into place
 xcopy S:\EFI\refind\refind_x64.efi bootmgfw.efi
 echo "Success"
